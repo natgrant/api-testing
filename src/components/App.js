e5991523e7cdd37be0eb18031b03919fd136dfa0
Nat Grant <email.natgrant@gmail.com>
2019-02-03 21:47:25 +1300

    accessing api

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    };
+  }
+  componentDidMount() {
+    var proxyUrl = "https://cors-anywhere.herokuapp.com/",
+      targetUrl = "https://www.metaweather.com/api/location/search/?query=san";
+    fetch(proxyUrl + targetUrl)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json
+        });
+      });
+  }
   render() {
+    var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
     return (
-      <div>
-        <h1>Welcome to my Starter App</h1>
+      <div className="App">
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              City: {item.title} | Where: {item.location_type}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
